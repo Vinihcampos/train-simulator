28ad4b201838dab845d5c70214ae2706d08e2ac8
Vitor Greati <greati@ufrn.edu.br>
2017-05-27 10:29:45 -0300

    Make panels work

--- client/panels.cpp
+++ client/panels.cpp
@@ -3,6 +3,7 @@
 #include <menu.h>
 #include <cstdlib>
 #include <cstring>
+#include <map>
 #include <iostream>
 
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
@@ -37,34 +38,122 @@ char * train_choices[] = {
 PANEL * panels[4];
 MENU * menus[3];
 WINDOW * windows[4];
+bool connected = false;
+int command = 0;
+std::map<PANEL *, int> panels_id;
 
 int n_general_choices = ARRAY_SIZE(general_choices);
 int n_train_choices = ARRAY_SIZE(train_choices);
 
 // Arrays of functions for each panel
-void (*gen_funcs[3])(void);					// Functions for general menu
-void (*train_mturn_funcs[3])(void);			// Functions for turn train menu
-void (*train_mspeed_funcs[3])(void);		// Functions for train speed menu
-void (*train_speed_funcs[3])(void);			// Function for train speed window
+void (*gen_funcs[3])(int);					// Functions for general menu
+void (*train_mturn_funcs[3])(int);			// Functions for turn train menu
+void (*train_mspeed_funcs[3])(int);		// Functions for train speed menu
+void (*train_speed_funcs[3])(int);			// Function for train speed window
 
 /*------------- Functions for each panel ----------------*/
 // General panel
-void up_general_menu();
-void down_general_menu();
-void choose_general_menu();
+//--------------------------------------------------------
+//-- Commands --//
+void up_general_menu(int);
+void down_general_menu(int);
+void choose_general_menu(int);
 
-void up_general_menu() {
-	menu_driver(menus[0], REQ_UP_ITEM);	
+void up_general_menu(int p) {
+	menu_driver(menus[p], REQ_UP_ITEM);	
 }
 
-void down_general_menu() {
-	menu_driver(menus[0], REQ_DOWN_ITEM);	
+void down_general_menu(int p) {
+	menu_driver(menus[p], REQ_DOWN_ITEM);	
 }
 
-void choose_general_menu() {
-	
+void choose_general_menu(int i) {
+	ITEM * cur = (ITEM *) current_item(menus[i]);
+	void (*p) (char *) = reinterpret_cast<void (*) (char *)>
+		(item_userptr(cur));	
+	if (p != nullptr)
+		p((char *) item_name(cur));
+}
+//-- Items actions --//
+void connection_management(char * name);
+void turn_on_all(char * name);
+void turn_off_all(char * name);
+void turn(char * name);
+void set_speed(char * name);
+void exit_client(char * name);
+
+void connection_management(char * name) {
+	bool before = connected;
+
+	if (!connected) {
+		connected = true;
+		general_choices[0] = "Disconnect";
+	} else {
+		connected = false;
+		general_choices[0] = "Connect";
+	}
+
+	if (before != connected) {
+		ITEM ** items = menu_items(menus[0]);
+		unpost_menu(menus[0]);
+		free_item(items[0]);
+		items[0] = new_item(general_choices[0], general_choices[0]);
+		set_item_userptr(items[0], reinterpret_cast<void *>(connection_management));
+		set_menu_items(menus[0], items);
+		post_menu(menus[0]);
+		wrefresh(windows[0]);
+	}
 }
 
+void turn_on_all(char * name) {
+
+}
+
+void turn_off_all(char * name) {
+
+}
+
+void turn(char * name) {
+	top_panel(panels[1]);
+}
+
+void set_speed(char * name) {
+	top_panel(panels[2]);
+}
+
+void exit_client(char * name) {
+	command = -1;
+	endwin();
+}
+//-----------------------------------
+// Train turn panel
+// ---------------------------------
+//--- Itens actions ---//
+void turn_train_menu(char * name);
+void exit_train_panel(char * name);
+
+void turn_train_menu(char * name) {
+
+}
+
+void exit_train_panel(char * name) {
+	top_panel(panels[0]);
+}
+
+//-----------------------------------
+// Train speed panel
+// ---------------------------------
+//--- Itens actions ---//
+void speed_train_menu(char * name);
+
+void speed_train_menu(char * name) {
+
+}
+
+
+
+/* Main method.
+ -----------------------------------------------*/
 int main(void) {
 	// Init ncurses
 	initscr();
@@ -79,6 +168,12 @@ int main(void) {
 	for (int i = 0; i < n_general_choices; ++i)
 		general_items[i] = new_item(general_choices[i], general_choices[i]);
 	general_items[n_general_choices] = (ITEM *) NULL;
+	set_item_userptr(general_items[0], reinterpret_cast<void *>(connection_management));
+	set_item_userptr(general_items[1], reinterpret_cast<void *>(turn_on_all));
+	set_item_userptr(general_items[2], reinterpret_cast<void *>(turn_off_all));
+	set_item_userptr(general_items[3], reinterpret_cast<void *>(turn));
+	set_item_userptr(general_items[4], reinterpret_cast<void *>(set_speed));
+	set_item_userptr(general_items[5], reinterpret_cast<void *>(exit_client));
 	menus[0] = new_menu((ITEM * *) general_items);
 	windows[0] = newwin(30,50,0,0);
 	keypad(windows[0], TRUE);
@@ -88,6 +183,7 @@ int main(void) {
 	post_menu(menus[0]);
 	wrefresh(windows[0]);
 	panels[0] = new_panel(windows[0]);
+	panels_id.insert(std::make_pair(panels[0], 0));
 	gen_funcs[DOWN_COMMAND] = down_general_menu;
 	gen_funcs[UP_COMMAND] = up_general_menu;
 	gen_funcs[CHOOSE_COMMAND] = choose_general_menu;
@@ -98,8 +194,12 @@ int main(void) {
 	ITEM * * train_speed_items = (ITEM **) calloc(n_train_choices + 1, sizeof(ITEM *));
 	for (int i = 0; i < n_train_choices; ++i) {
 		train_turn_items[i] = new_item(train_choices[i], "ON");
+		set_item_userptr(train_turn_items[i], reinterpret_cast<void *>(turn_train_menu));
 		train_speed_items[i] = new_item(train_choices[i], "100");
+		set_item_userptr(train_speed_items[i], reinterpret_cast<void *>(speed_train_menu));
 	}
+	set_item_userptr(train_turn_items[7], reinterpret_cast<void *>(exit_train_panel));
+	set_item_userptr(train_speed_items[7], reinterpret_cast<void *>(exit_train_panel));
 	train_turn_items[n_train_choices] = (ITEM *) NULL;
 	train_speed_items[n_train_choices] = (ITEM *) NULL;
 	menus[1] = new_menu((ITEM * *) train_turn_items);
@@ -120,8 +220,17 @@ int main(void) {
 	wrefresh(windows[2]);
 	panels[1] = new_panel(windows[1]);
 	panels[2] = new_panel(windows[2]);
+	panels_id.insert(std::make_pair(panels[1], 1));
+	panels_id.insert(std::make_pair(panels[2], 2));
 	set_panel_userptr(panels[1], train_mturn_funcs);
 	set_panel_userptr(panels[2], train_mspeed_funcs);
+	train_mturn_funcs[DOWN_COMMAND] = down_general_menu;
+	train_mturn_funcs[UP_COMMAND] = up_general_menu;
+	train_mturn_funcs[CHOOSE_COMMAND] = choose_general_menu;
+	train_mspeed_funcs[DOWN_COMMAND] = down_general_menu;
+	train_mspeed_funcs[UP_COMMAND] = up_general_menu;
+	train_mspeed_funcs[CHOOSE_COMMAND] = choose_general_menu;
+
 
 	top_panel(panels[0]);
 
@@ -129,24 +238,24 @@ int main(void) {
 	doupdate();
 	refresh();
 	/*------------ Main loop ---------------*/
-	// The key pressed
-	int command = 0;
-
 	while (true) {
 		PANEL * top = panel_below((PANEL *)0);
 		command = wgetch(panel_window(top));
-		void (**funcs)(void) = reinterpret_cast<void (**)(void)>(const_cast<void *>(panel_userptr(top)));
+		void (**funcs)(int) = reinterpret_cast<void (**)(int)>(const_cast<void *>(panel_userptr(top)));
 		switch(command) {	
 			case KEY_DOWN: {
-					funcs[DOWN_COMMAND]();
+					if (funcs[DOWN_COMMAND])
+						funcs[DOWN_COMMAND](panels_id[top]);
 				break;
 			}
 			case KEY_UP: {
-					funcs[UP_COMMAND]();
+					if (funcs[UP_COMMAND])
+						funcs[UP_COMMAND](panels_id[top]);
 				break;
 		    }
 			case 10: {
-					funcs[CHOOSE_COMMAND]();
+					if (funcs[CHOOSE_COMMAND])
+						funcs[CHOOSE_COMMAND](panels_id[top]);
 				break;		 
 			}
 		}
